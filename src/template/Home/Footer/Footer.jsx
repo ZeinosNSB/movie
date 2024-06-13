ebca15c4b5cdfcce9fb8417e566ac7d97f3b7005
zeinosnsb <chinhthantoc@gmail.com>
2024-06-14 00:41:36 +0700

    first-commit

--- src/template/Home/Footer/Footer.jsx
+++ src/template/Home/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import { MoveRight } from 'lucide-react'
 
 import cybersoft from '@/assets/logo/cybersoft.png'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 function Footer() {
   return (
     <footer className='w-full'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
         <div className='py-14 grid grid-cols-12 gap-x-5 gap-y-8'>
-          <div className='col-span-full xl:col-span-3 relative bg-gradient-to-tl from-amber-600 to-orange-800 rounded-2xl gap-12 p-6 xl:w-72 h-96 flex flex-col justify-center items-center'>
+          <div className='col-span-full xl:col-span-3 relative bg-slate-500 rounded-2xl gap-12 p-6 xl:w-72 h-96 flex flex-col justify-center items-center'>
             <a href='#' className='flex justify-center lg:justify-start'>
               <img className='h-32' src={cybersoft} alt='CyberSoft' />
             </a>
@@ -16,7 +18,7 @@ function Footer() {
             </p>
           </div>
 
-          <div className='block text-center xl:text-left xl:py-16 col-span-full min-[500px]:col-span-6 md:col-span-4 xl:col-span-3 xl:pl-5'>
+          <div className='block text-center text-sm xl:text-left xl:py-16 col-span-full min-[500px]:col-span-6 md:col-span-4 xl:col-span-3 xl:pl-5'>
             <h4 className='text-lg text-gray-900 font-bold mb-9'>Get In Touch</h4>
             <ul className='text-gray-900 transition-all duration-500 grid gap-6'>
               <li>support@pagedone.com</li>
@@ -26,7 +28,7 @@ function Footer() {
           </div>
           <div className='block xl:py-16 col-span-full min-[500px]:col-span-6 md:col-span-4 xl:col-span-3'>
             <h4 className='text-lg text-gray-900 font-bold mb-9 text-center xl:text-left'>Quick Links</h4>
-            <div className='flex gap-6 xl:gap-12 max-xl:justify-center'>
+            <div className='flex text-sm gap-6 xl:gap-12 max-xl:justify-center'>
               <ul className='text-gray-600 transition-all duration-500 grid gap-6'>
                 <li>
                   <a href='#'>Home</a>
@@ -60,19 +62,11 @@ function Footer() {
           <div className='block xl:py-16 col-span-full md:col-span-4 xl:col-span-3'>
             <h4 className='text-lg text-gray-900 font-bold mb-9 text-center xl:text-left'>Newsletter</h4>
             <div className='grid gap-7 '>
-              <input
-                type='text'
-                name='email'
-                className='py-2 px-4 border border-gray-300 shadow-sm h-14 text-lg text-gray-800 rounded-full w-full  xl:w-64 placeholder:text-gray-400 focus:outline-none'
-                placeholder='Enter email..'
-              />
-              <button
-                type='submit'
-                className='flex gap-2 justify-center items-center py-3.5 px-7 rounded-full text-white bg-gradient-to-tl from-amber-600 to-orange-800 shadow-md w-fit transition-all duration-500 mx-auto xl:mx-0'
-              >
+              <Input type='email' placeholder='Enter email ...' />
+              <Button type='submit'>
                 Subscribe
-                <MoveRight />
-              </button>
+                <MoveRight className='pl-2' />
+              </Button>
             </div>
           </div>
         </div>
